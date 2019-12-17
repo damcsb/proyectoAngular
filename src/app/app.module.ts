b92e1d11f764b0a1bce6f5efc110ebb135129984
damcsb1 damcsb1 <hola1212@temp-link.net>
2019-12-17 12:44:13 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+
+import { TopicsComponent } from './componentes/topics/topics.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArticulosComponent } from './componentes/articulos/articulos.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopicsComponent,
+    ArticulosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
